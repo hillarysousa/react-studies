64f9f274ceb784494b41c8cfb43a3e5a9a824259
Hillary Sousa <hillaryhpf@gmail.com>
2018-02-08 16:18:05 -0200

    State adicionado à classe

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 // { Component } pega a classe Component do react e a traz como variavel, podendo ser usada sem React.Component
-
-/* const SearchBar = () => {
-  return <input />;
-}; */
 
 // Cria uma nova classe chamada SearchBar e dá acesso a todas as funcionalidades da classe de componentes no React
 class SearchBar extends Component {
+  // A função constructor é a primeira e unica a ser chamada automaticamente quando uma nova instancia da classe (no caso, SearchBar) é criada
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+  }
+
   render() {
-    return <input />;
+    return (
+      <div>
+        <input onChange={event => this.setState({term: event.target.value})} />
+        {/* toda vez que o valor do input muda, ele atualiza o estado da classe. */}
+        <p>Valor do input: {this.state.term}</p>
+      </div>
+    );
   }
 }
 
